05b9b2df1631c424b0ce008cdc5829cb8cec2ea2
Rinat Sarmuldin <ura07srr@gmail.com>
2024-01-08 20:31:25 +0500

    Cleaning the project, moving the layout. We break the project into components. Step#1

---
+++ src/components/WeatherSummary.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="summary">
+    <div
+        style="background-image: url('./img/weather-main/thunderstorm.png');"
+        class="pic-main"
+    ></div>
+    <div class="weather">
+      <div class="temp">
+        14 °C
+      </div>
+      <div class="weather-desc text-block">
+        Thunderstorm
+      </div>
+    </div>
+    <div class="city text-block">
+      Paris,
+      FR
+    </div>
+    <div class="date text-block">
+      Monday, January 8, 2024
+    </div>
+  </div>
+</template>
+
+<style scoped lang="sass">
+@import "../assets/styles/main"
+
+
+</style>
